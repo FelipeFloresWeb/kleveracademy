ce58451de78a00cbccc9df3894747904b599f76a
Felipe <felipefloresvendas@gmail.com>
2022-05-24 15:30:38 -0300

    feat: add more klever-academy host

--- kleverAcademyBack/settings/base.py
+++ kleverAcademyBack/settings/base.py
@@ -29,7 +29,8 @@ SECRET_KEY = getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['intense-ridge-56931.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['intense-ridge-56931.herokuapp.com',
+                 '127.0.0.1', 'klever-academy.netlify.app']
 
 # Application definition
 

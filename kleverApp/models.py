0fca67b8d94f1c6a7505a0bcf08009572d05d60f
Felipe <felipefloresvendas@gmail.com>
2022-06-16 12:11:54 -0300

    feat: add videos model

--- kleverApp/models.py
+++ kleverApp/models.py
@@ -1,22 +1,17 @@
-# from django.db import models
+from django.db import models
 
 
-# class User(models.Model):
-#     name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
-#     email = models.EmailField(max_length=255, unique=True)
-#     password = models.CharField(max_length=100)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
+class Videos(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    video_url = models.CharField(max_length=200)
+    thumbnail_url = models.CharField(max_length=200)
+    rate = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-#     def __str__(self):
-#         return self.email
+    def __str__(self):
+        return self.title
 
 
-# class Course(models.Model):
-#     name = models.CharField(max_length=100)
-#     views = models.IntegerField(default=0)
-#     likes = models.IntegerField(default=0)
-#     description = models.TextField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)

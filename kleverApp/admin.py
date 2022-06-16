e420c581def50881717b983382aa59e89f73b69e
Felipe <felipefloresvendas@gmail.com>
2022-06-16 15:00:13 -0300

    feat: add viewer for FavoriteVideo

--- kleverApp/admin.py
+++ kleverApp/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Videos
+from .models import Videos, FavoriteVideo
 
 
 @admin.register(Videos)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'video_url', 'thumbnail_url', 'rate', 'likes', 'created_at', 'updated_at')
+
+@admin.register(FavoriteVideo)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user', 'video', 'id')
